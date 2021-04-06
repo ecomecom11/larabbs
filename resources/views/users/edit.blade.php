3e22ccdea130b4c4bc9c8d3d2e082318ca7f8b25
Emily <ecomecom11@gmail.com>
2021-04-06 16:56:01 +0000

    Upload User Image

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -14,31 +14,40 @@
 
       <div class="card-body">
 
-        <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8">
-          <input type="hidden" name="_method" value="PUT">
-          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <form action="{{ route('users.update', $user->id) }}" method="POST" accept-charset="UTF-8" enctype="multipart/form-data">          
+            <input type="hidden" name="_method" value="PUT">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-          @include('shared._error')
+            @include('shared._error')
 
-          <div class="form-group">
-            <label for="name-field">User Name</label>
-            <input class="form-control" type="text" name="name" id="name-field" value="{{ old('name', $user->name) }}" />
-          </div>
-          <div class="form-group">
-            <label for="email-field">E-mail</label>
-            <input class="form-control" type="text" name="email" id="email-field" value="{{ old('email', $user->email) }}" />
-          </div>
-          <div class="form-group">
-            <label for="introduction-field">Personal Information</label>
-            <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
-          </div>
-          <div class="well well-sm">
-            <button type="submit" class="btn btn-primary">Save</button>
-          </div>
-        </form>
-      </div>
+            <div class="form-group">
+                <label for="name-field">User Name</label>
+                <input class="form-control" type="text" name="name" id="name-field" value="{{ old('name', $user->name) }}" />
+            </div>
+            <div class="form-group">
+                <label for="email-field">E-mail</label>
+                <input class="form-control" type="text" name="email" id="email-field" value="{{ old('email', $user->email) }}" />
+            </div>
+            <div class="form-group">
+                <label for="introduction-field">Personal Information</label>
+                <textarea name="introduction" id="introduction-field" class="form-control" rows="3">{{ old('introduction', $user->introduction) }}</textarea>
+            </div>
+            <div class="form-group mb-4">
+                <label for="" class="avatar-label">User image</label>
+                <input type="file" name="avatar" class="form-control-file">
+
+                @if($user->avatar)
+                <br>
+                <img class="thumbnail img-responsive" src="{{ $user->avatar }}" width="200" />
+                @endif
+            </div>
+            <div class="well well-sm">
+                <button type="submit" class="btn btn-primary">Save</button>
+            </div>
+            </form>
+        </div>
+        </div>
     </div>
-  </div>
 </div>
 
 @endsection
