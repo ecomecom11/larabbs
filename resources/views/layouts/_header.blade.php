9508cc7c39c07956e525aa9ee11531612b3c3f56
Emily <ecomecom11@gmail.com>
2021-04-06 12:39:21 +0000

    Fix redirect route after signup

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,25 +1,45 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
-  <div class="container">
+<div class="container">
     <!-- Branding Image -->
     <a class="navbar-brand " href="{{ url('/') }}">
-      LaraBBS
+    LaraBBS
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Left Side Of Navbar -->
-      <ul class="navbar-nav mr-auto">
+    <!-- Left Side Of Navbar -->
+    <ul class="navbar-nav mr-auto">
 
-      </ul>
+    </ul>
 
-      <!-- Right Side Of Navbar -->
-      <ul class="navbar-nav navbar-right">
+    <!-- Right Side Of Navbar -->
+    <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
+        @guest
         <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
-      </ul>
+        @else
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://icons.iconarchive.com/icons/graphicloads/flat-finance/32/person-icon.png">
+            {{ Auth::user()->name }}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="">User Centre</a>
+            <a class="dropdown-item" href="">Edit Information</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" id="logout" href="#">
+                <form action="{{ route('logout') }}" method="POST">
+                {{ csrf_field() }}
+                <button class="btn btn-block btn-danger" type="submit" name="button">Logout</button>
+                </form>
+            </a>
+            </div>
+        </li>
+        @endguest
+    </ul>
     </div>
-  </div>
+</div>
 </nav>
